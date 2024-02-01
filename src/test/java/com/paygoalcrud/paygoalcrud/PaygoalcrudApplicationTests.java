3f84ca6aa1123e43bec6c02c6fd6e715087a04e3
asgonzales <asgonzalesromani@gmail.com>
2024-02-01 15:30:16 -0300

    first commit

---
+++ src/test/java/com/paygoalcrud/paygoalcrud/PaygoalcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paygoalcrud.paygoalcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaygoalcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
